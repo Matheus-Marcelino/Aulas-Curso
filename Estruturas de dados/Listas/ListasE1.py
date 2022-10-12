251a7247df39f5963f998286f0cc6866b7fa6bd2
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-12 17:22:53 -0300

    Exercicio terminado

--- Estruturas de dados/Listas/ListasE1.py
+++ Estruturas de dados/Listas/ListasE1.py
@@ -1,10 +1,27 @@
 from colorama import init
 init()
 
-lista_NUM = list()
+lista_NUM = []
+maior = menor = 0
 
 for cont in range(0, 5):
-    lista_NUM.append(int(input(f'Digite um valor na posição{cont}: ')))
+    lista_NUM.append(int(input(f'Digite um valor na posição {cont}: ')))
+    if cont == 0:
+        maior = menor = lista_NUM[0]
+    else:
+        if lista_NUM[cont] > maior:
+            maior = lista_NUM[cont]
+        if lista_NUM[cont] < menor:
+            menor = lista_NUM[cont]
 
-print(f'\nO maior valor digitado foi \033[1;32m{max(lista_NUM)}\033[m')
-print(f'O menor valor digitado foi \033[1;32m{min(lista_NUM)}\033[m')
+print(f'\nO maior valor digitado foi \033[1;32m{maior}\033[m na posição', end='')
+
+for indice, valor in enumerate(lista_NUM):
+    if valor == maior:
+        print(f' {indice}...', end='')
+
+print(f'\nO menor valor digitado foi \033[1;32m{menor}\033[m na posição', end='')
+
+for indice, valor in enumerate(lista_NUM):
+    if valor == menor:
+        print(f' {indice}...', end='')
