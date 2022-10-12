73273bf00cccc46361805d77a416422eecc359b9
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-12 17:09:44 -0300

    Primeira parte

--- Estruturas de dados/Listas/ListasE1.py
+++ Estruturas de dados/Listas/ListasE1.py
@@ -3,8 +3,8 @@ init()
 
 lista_NUM = list()
 
-for dados in range(0, 5):
-    lista_NUM.append(int(input("Digite um valor: ")))
+for cont in range(0, 5):
+    lista_NUM.append(int(input(f'Digite um valor na posição{cont}: ')))
 
 print(f'\nO maior valor digitado foi \033[1;32m{max(lista_NUM)}\033[m')
 print(f'O menor valor digitado foi \033[1;32m{min(lista_NUM)}\033[m')
