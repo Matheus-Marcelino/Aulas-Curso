59383926449afac9731881afde61dffaacbddeab
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-12 15:06:20 -0300

    Adicionando os comentarios sobre os processos

--- Variaveis/Tuplas/TuplasE6.py
+++ Variaveis/Tuplas/TuplasE6.py
@@ -2,9 +2,9 @@
 
 PALAVRAS = ('Matheus', 'Maria', 'Josh', 'Kauan')
 
-for palavra in PALAVRAS:
+for palavra in PALAVRAS:  # Acessando as palavras
     print(f'\nNa palavra {palavra.upper()} temos: ', end='')
-    for letra in palavra:
-        if letra.lower() in 'aeiou':
+    for letra in palavra:  # acessando as letras
+        if letra.lower() in 'aeiou':  # especificando as vogais
             print(letra, end=' ')
 print()
