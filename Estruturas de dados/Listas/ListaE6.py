57e8fb2bc0360ac985b2ad84e321fc956c978c30
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 18:15:43 -0300

    Codigo corrigido e finalizado

--- Estruturas de dados/Listas/ListaE6.py
+++ Estruturas de dados/Listas/ListaE6.py
@@ -1,18 +1,20 @@
-lista = lista_im = lista_par = list()
-
+lista, lista_im, lista_par = list(), list(), list()
 decisao = int(input('Quantos numeros deseja digitar [0 finaliza]: '))
 
 if decisao != 0:
     for _ in range(decisao):
         lista.append(int(input('Digite um valor: ')))
         print('adicionando a lista')
-    
-    for cont in range(len(lista)):
+
+    for cont in lista:
+        print(lista)
+        print(cont)
         if cont % 2 == 0:
             lista_par.append(cont)
         else:
             lista_im.append(cont)
 
-print(f'A lista Com todos os digitos é: {lista}')
-print(f'Os numeros pares digitados foram: {lista_par}')
-print(f'Os numeros impares digitados foram: {lista_im}')
+    print(f'A lista Com todos os digitos é: {lista}')
+    print(f'Os numeros pares digitados foram: {lista_par}')
+    print(f'Os numeros impares digitados foram: {lista_im}')
+print('Obrigado por usar o programa')
