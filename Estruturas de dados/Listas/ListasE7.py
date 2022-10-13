8492264fc8ec49d4cc07a48c5b4ca7f514daf414
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 18:45:11 -0300

    Exercicio 7 finalizado

--- Estruturas de dados/Listas/ListasE7.py
+++ Estruturas de dados/Listas/ListasE7.py
@@ -1,10 +1,10 @@
 lista_dir, lista_esq = list(), list()
 expressao = str(input('digites uma expressão: '))
-for c in expressao:
-    if '(' in c:
-        lista_dir.append(c)
-    elif ')' in c:
-        lista_esq.append(c)
+for simbolo in expressao:
+    if '(' in simbolo:
+        lista_dir.append(simbolo)
+    elif ')' in simbolo:
+        lista_esq.append(simbolo)
 
 if len(lista_dir) == len(lista_esq):
     print('Sua expressão está correta!')
