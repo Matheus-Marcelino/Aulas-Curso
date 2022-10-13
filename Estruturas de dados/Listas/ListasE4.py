fec0641b3e0cf35a03822f587e96d12aae87c8af
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-13 17:30:19 -0300

    Lista em ordem crescente finalizada

--- Estruturas de dados/Listas/ListasE4.py
+++ Estruturas de dados/Listas/ListasE4.py
@@ -1,7 +1,14 @@
 lista_num = list()
 
-for _ in range(0, 5):
-    numeros = int(input('Digite um valor: '))
+for c in range(0, 5):
+    numero = int(input('Digite um valor: '))
     print('Adicionando a lista')
-    lista_num.append(numeros)
+    if c == 0 or numero > lista_num[-1]:
+        lista_num.append(numero)
+    else:
+        for pos in range(len(lista_num)):
+            if numero <= lista_num[pos]:
+                lista_num.insert(pos, numero)
+                break
 
+print(f'\nOs dados digitados em ordem crescente é: {lista_num}')
