790d811c3b5fbf1d83489fc27b60058521e389a8
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-15 15:43:17 -0300

    Finalizando

--- Estruturas de dados/Listas/Listas2E2.py
+++ Estruturas de dados/Listas/Listas2E2.py
@@ -7,5 +7,7 @@ for _ in range(0, 7):
     else:
         lista_NUM[0].append(numero)
 
+lista_NUM[0].sort()
+lista_NUM[1].sort()
 print(f'Os numeros pares são: {lista_NUM[1]}')
 print(f'Os numeros impares são: {lista_NUM[0]}')
