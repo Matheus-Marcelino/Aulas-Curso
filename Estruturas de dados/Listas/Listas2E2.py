899eb596ccd19f43e9a0548b676fc0af403b78d0
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-15 15:38:39 -0300

    Exercicio completo

---
+++ Estruturas de dados/Listas/Listas2E2.py
@@ -0,0 +1,11 @@
+lista_NUM = [[], []]
+
+for _ in range(0, 7):
+    numero = int(input('Digite um valor: '))
+    if numero % 2 == 0:
+        lista_NUM[1].append(numero)
+    else:
+        lista_NUM[0].append(numero)
+
+print(f'Os numeros pares são: {lista_NUM[1]}')
+print(f'Os numeros impares são: {lista_NUM[0]}')
