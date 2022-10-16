2f86d9cb33a7cc610240e33022a4f64a10383925
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-16 14:27:33 -0300

    Terminado

--- Estruturas de dados/Dicionarios/DicionarioAULA.py
+++ Estruturas de dados/Dicionarios/DicionarioAULA.py
@@ -1,7 +1,7 @@
 pessoas = {'nome': 'Matheus', 'sexo': 'M', 'idade': '22'}
 print(f'O {pessoas["nome"]} tem {pessoas["idade"]} anos.')
 print(pessoas.values())  # mostra os valores das keys como [Matheus, M, 22]
-print(pessoas.keys())  #mostra as keys como [nome, sexo, idade ]
+print(pessoas.keys())  # mostra as keys como [nome, sexo, idade ]
 print(pessoas.items())  # mostra O dicionario todo com keys e valores
 print('\n', '-=' * 40, '\n')
 """
@@ -17,7 +17,7 @@ for key in pessoas.keys():
 
 for value in pessoas.values():
     print(value)
-    
+
 for key, value in pessoas.items():
     print(f'{key} = {value}')
 
@@ -34,17 +34,17 @@ print(estado2)
 print(brasil)
 print(brasil[0])
 print(brasil[1])
-print(brasil[0]['uf'])  # mostra o value de uf do elemento 0 
-print(brasil[1]['sigla']) # mostra o value de sigla do elemento 1
+print(brasil[0]['uf'])  # mostra o value de uf do elemento 0
+print(brasil[1]['sigla'])  # mostra o value de sigla do elemento 1
 print('\n', '-=' * 40, '\n')
 
 brasil.clear()
 estado = dict()
 for c in range(0, 3):
-    estado['uf'] = str(input('Unidade federativa: '))    
+    estado['uf'] = str(input('Unidade federativa: '))
     estado['sigla'] = str(input('Sigla do estado: '))
     brasil.append(estado.copy())  # .copy() substitiu o [:]
-    
+
 for estate in brasil:  # varrendo a lista
     for key, value in estate.items():  # varrendo os dicionarios
         print(f'O campo {key} tem valor {value}')
