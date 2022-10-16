adfc01541d3daa083494f957c327a3a0aeeede2a
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-16 12:57:16 -0300

    Exercicio 6 finalizado

--- Estruturas de dados/Listas/Listas2E6.py
+++ Estruturas de dados/Listas/Listas2E6.py
@@ -1,7 +1,25 @@
-lista_temp, lista_users = list(), list()
-decisao = int(input('Quantos alunos deseja cadrastar [0 finaliza]: '))
+ficha = list()
+while True:
+    decisao = int(input('Quantos alunos deseja cadrastar [0 finaliza]: '))
+    print()
 
-for c in range(decisao):
-    lista_temp.append(str(input('Nome: ')))
-    lista_temp.append(float(input('peso: ')))
-    print(f'Boletim do')
+    for _ in range(decisao):
+        nome = str(input('Nome: '))
+        nota1 = float(input('Nota 1: '))
+        nota2 = float(input('Nota 2: '))
+        media = (nota1 + nota2) / 2
+        ficha.append([nome, [nota1, nota2], media])
+        print(f'Boletim do {ficha[0][0]} foi processado!\n')
+
+    for pos, dado in enumerate(ficha):
+        print(f'{pos} {dado[0]} {dado[2]}')
+
+    while True:
+        aluno = int(input('\ndeseja ver a nota de algum aluno?[999 finaliza] '))
+        if aluno == 999:
+            break
+        else:
+            print(ficha[aluno][1])
+    
+    if aluno == 999:
+            break
