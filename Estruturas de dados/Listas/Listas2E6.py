635d42aeeb76ec427c9a00a88410ad36969b0520
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-16 11:47:14 -0300

    Criando o exercicio 6

---
+++ Estruturas de dados/Listas/Listas2E6.py
@@ -0,0 +1,7 @@
+lista_temp, lista_users = list(), list()
+decisao = int(input('Quantos alunos deseja cadrastar [0 finaliza]: '))
+
+for c in range(decisao):
+    lista_temp.append(str(input('Nome: ')))
+    lista_temp.append(float(input('peso: ')))
+    print(f'Boletim do')
