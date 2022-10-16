867a63d944ea8f9f3ee4a9adc1f5be8613a2eed8
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-16 17:59:50 -0300

    Finalizando

--- Estruturas de dados/Dicionarios/DicionarioE1.py
+++ Estruturas de dados/Dicionarios/DicionarioE1.py
@@ -4,9 +4,9 @@ User['nome'] = str(input('Nome: ')).capitalize()
 User['media'] = float(input(f'Média de {User["nome"]}: '))
 
 print('-=' * 40)
-print(f'Nome é igual a {User["nome"]}')
-print(f'Média é igual a {User["media"]}')
-if User['media'] >= 5:
-    print('Situação é igual a Aprovado')
+print(f' - Nome é igual a {User["nome"]}')
+print(f' - Média é igual a {User["media"]}')
+if User['media'] >= 5.0:
+    print(' - Situação é igual a Aprovado')
 else:
-    print('Situação é igual a Reprovado')
+    print(' - Situação é igual a Reprovado')
