b88b160b8df846525adf8e69bf1952a0c796ea48
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-18 18:34:13 -0300

    Terminando o exercicio

--- Funções/DefE3.py
+++ Funções/DefE3.py
@@ -8,7 +8,7 @@ from time import sleep
 def contador(inicio, fim, pulos=1):
     if pulos == 0:
         pulos = 1
-
+    
     print('-=' * 40)
     print(f'Contagem de {inicio} até {fim} pulando de {pulos} em {pulos}')
     for cont in range(inicio, fim - 1, pulos):
@@ -19,7 +19,7 @@ def contador(inicio, fim, pulos=1):
             print(f'{cont}', end='', flush=True)
             sleep(0.5)
     print('FIM')
-    print('-=' * 40, '\n')
+    print('\n')
 
 
 contador(1, 11, 1)
@@ -37,6 +37,12 @@ from time import sleep
 
 
 def contador(i, f, p):
+    print('-=' * 20)
+    
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
     print(f'Contagem de {i} até {f} pulando de {p} em {p}')
     if i < f:
         cont = i
@@ -54,3 +60,10 @@ def contador(i, f, p):
         print('FIM')
 
 
+contador(1, 11, 1)
+contador(12, -10)
+print('Agora é a sua vez de personalizar a contagem!')
+inicio = int(input('Inicio: '))
+fim = int(input('fim: '))
+pulos = int(input('pulos: '))
+contador(inicio, fim, pulos)
