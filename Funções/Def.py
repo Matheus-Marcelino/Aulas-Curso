20e47ad29d5e78bb672e3c0f4362261159292178
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-18 15:01:00 -0300

    Iniciando a aula de funcao

--- Funções/Def.py
+++
@@ -1,9 +0,0 @@
-def titulo(txt):
-    print('-' * 30)
-    print(txt)
-    print('-' * 30)
-
-
-titulo('APRENDENDO PYTHON')
-titulo('MINHA VIDA É UM CU')
-titulo('QUE SACO')
---
+++ Funções/DefAULA.py
@@ -0,0 +1,4 @@
+def frufru(txt):
+    print('-=' * 40)
+    print(txt)
+    print('-=' * 40)
