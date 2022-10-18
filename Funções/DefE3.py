ccd84011a573c4f86e1c7a72f72a161c3f9225d3
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-18 18:24:46 -0300

    Começando a segnda aprte

--- Funções/DefE3.py
+++ Funções/DefE3.py
@@ -1,3 +1,10 @@
+"""
+Fazendo o exercicio de uma maneira mais inteligente
+porque ele não especificou :)
+
+from time import sleep
+
+
 def contador(inicio, fim, pulos=1):
     if pulos == 0:
         pulos = 1
@@ -6,9 +13,11 @@ def contador(inicio, fim, pulos=1):
     print(f'Contagem de {inicio} até {fim} pulando de {pulos} em {pulos}')
     for cont in range(inicio, fim - 1, pulos):
         if cont < 0:
-            print(f'{cont}', end=' ')
+            print(f'{cont}', end='', flush=True)
+            sleep(0.5)
         else:
-            print(cont, end=' ')
+            print(f'{cont}', end='', flush=True)
+            sleep(0.5)
     print('FIM')
     print('-=' * 40, '\n')
 
@@ -21,3 +30,27 @@ fim = int(input('fim: '))
 pulos = int(input('pulos: '))
 
 contador(inicio, fim, pulos)
+"""
+
+# exercicio como ele fez
+from time import sleep
+
+
+def contador(i, f, p):
+    print(f'Contagem de {i} até {f} pulando de {p} em {p}')
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont}', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('FIM')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont}', end='', flush=True)
+            sleep(0.5)
+            cont -= p
+        print('FIM')
+
+
