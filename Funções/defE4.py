1707d30bbfd28483cd76cc1dd529ed3b8f01639d
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-19 14:45:54 -0300

    Exercicio completo

--- Funções/defE4.py
+++ Funções/defE4.py
@@ -1,3 +1,6 @@
+"""
+maneira de como eu fiz o exercicio
+
 def maior():
     print(f'O maior numero foi {max(numeros)}')
     print(f'O menor numero foi {min(numeros)}')
@@ -10,3 +13,20 @@ for _ in range(quantidade):
     numeros.append(int(input('Digite um valor: '))) 
 
 maior()
+"""
+from time import sleep
+
+
+def maior(*num):
+    print('-=' * 40)
+    print('Analisando os valores passados...')
+    for c in num:
+        print(f'{c}', end=' ', flush=True)
+        sleep(0.5)
+    print(f'Foram informados {len(num)} valores no total')
+    print(f'O maior valor foi {max(num)}')
+
+
+maior(4, 5, 2, 1)
+maior(4, 5, 5, 3, 40)
+maior(1, 0, 5)
