9dc22871f848bf6cf85288b55332fe2792a7d2e4
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-21 19:56:24 -0300

    Exercicio terminado

---
+++ Funções/def2E1.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+
+def voto(idade):
+    print(date.today().year)
+    idade = date.today().year - idade
+    if 65 > idade >= 18:
+        print(f'Com {idade} anos: VOTO OBRIGATÓRIO.')
+    elif 15 < idade < 18 or idade >= 65:
+        print(f'Com {idade} anos: VOTO OPCIONAL.')
+    else:
+        print(f'Com {idade} anos: NÃO VOTA.')
+
+
+idade = int(input('Em que ano você nasceu: '))
+voto(idade)
