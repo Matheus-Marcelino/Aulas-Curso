dece889ecfd5ff4f998fea6c4a6add798add37b4
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-24 17:35:50 -0300

    Finalizando o exercicio 2

--- Funções/def2E2.py
+++ Funções/def2E2.py
@@ -1,2 +1,17 @@
+from time import sleep
+
+
 def Fatorial(numero, show=False):
-    
+    numero
+    fat = cont = 1
+    while cont <= numero:
+        if show:
+            sleep(0.5)
+            print(cont, flush=show, end=' > ')
+        fat = fat*cont
+        cont += 1
+
+    print(f'O valor de {numero}! eh = ' + str(fat))
+
+print('-' * 40)
+Fatorial(5)
