daded8686ae7d4fa4f9747686387ef7d63c26bca
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-24 18:11:28 -0300

    Terminando o exercicio da minha forma

---
+++ Funções/Def2E3.py
@@ -0,0 +1,16 @@
+def Ficha() -> None:
+    jogador = str(input('Nome do jogador: ')).strip().title()
+    gols = str(input('Quantidade de gols: ')).strip()
+    try:
+        gols = int(gols)
+    except ValueError:
+        gols = 0
+
+    if jogador == '' or jogador == ' ':
+        jogador = '<desconhecido>'
+
+    print(f'O jogador {jogador} fez {gols} gol(s) no campeonato.')
+
+
+Ficha()
+g
