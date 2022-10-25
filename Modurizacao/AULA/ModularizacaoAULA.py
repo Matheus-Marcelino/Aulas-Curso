d1b84ccd0ef8ba02613119013c08afc1a5ddc77d
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-25 17:18:57 -0300

    Exercicio finalizado

--- Modurizacao/AULA/ModularizacaoAULA.py
+++ Modurizacao/AULA/ModularizacaoAULA.py
@@ -1,4 +1,7 @@
+import uteis
+
 num = int(input('Digite um valor: '))
-fat = fatorial(num)
+fat = uteis.fatorial(num)
 print(f'O fatorial de {num} é {fat}')
-print(f'O dobre de {num} é {dobro(num)}')
+print(f'O dobro de {num} é {uteis.dobro(num)}')
+print(f'O triplo de {num} é {uteis.triplo(num)}')
