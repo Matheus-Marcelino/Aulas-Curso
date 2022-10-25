6bf660e771f63fa28f7392eb3abc48dec55f0551
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-25 17:32:43 -0300

    Adicionando os modulos

--- Modurizacao/ModularizacaoAULA.py
+++ Modurizacao/ModularizacaoAULA.py
@@ -1,7 +1,17 @@
 import uteis
+# from uteis import * -> importa tudo
+# from uteis import fatorial, dobro, uteis -> importa separadamente
 
 num = int(input('Digite um valor: '))
 fat = uteis.fatorial(num)
 print(f'O fatorial de {num} é {fat}')
 print(f'O dobro de {num} é {uteis.dobro(num)}')
 print(f'O triplo de {num} é {uteis.triplo(num)}')
+"""
+|
+|
+---> Pacotes são as pastas 
+        |
+        |
+        ---> Modulos são os arquivos
+"""
