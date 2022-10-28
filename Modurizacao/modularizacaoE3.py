e0828b8df8a6bec489efc614752ff43c814fc10a
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 16:53:25 -0300

    Finalizado

--- Modurizacao/modularizacaoE3.py
+++ Modurizacao/modularizacaoE3.py
@@ -1,12 +1,12 @@
-from moedaE3 import moeda
+from moedaE3 import moeda as md
 
 preco = float(input('Digite o preço: R$'))
 
 print(
-    f'O seu valor de {moeda.moeda(preco)} dobrado fica: {moeda.dobro(preco)}')
+    f'O seu valor de {md.moeda(preco)} dobrado fica: {md.dobro(preco)}')
 print(
-    f'O seu valor de {moeda.moeda(preco)} pela metade fica: {moeda.metade(preco)}')
+    f'O seu valor de {md.moeda(preco)} pela metade fica: {md.metade(preco)}')
 print(
-    f'O seu valor de {moeda.moeda(preco)} aumentado em 10% fica: {moeda.aumentar(preco, 10)}')
+    f'O seu valor de {md.moeda(preco)} aumentado em 10% fica: {md.aumentar(preco, 10)}')
 print(
-    f'O seu valor de {moeda.moeda(preco)} diminuido em 15% fica: {moeda.diminuir(preco, 15)}')
+    f'O seu valor de {md.moeda(preco)} diminuido em 15% fica: {md.diminuir(preco, 15)}')
