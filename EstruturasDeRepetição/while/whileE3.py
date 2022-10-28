5b6d9ac4bcc19cf0c351033401cb12fde2fcc98b
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 15:20:17 -0300

    Finalizado

--- EstruturasDeRepetição/while/whileE3.py
+++ EstruturasDeRepetição/while/whileE3.py
@@ -7,24 +7,24 @@ print(f'o fatorial de {n} é {factorial(n)}')
 
 '''Forma tradicional'''
 
-tot = 1 
+tot = 1
 c = n
 print(f'{n}! = ', end='')
 while c > 0:
-    print(f'{c}', end ='')
-    print(' x ' if c > 1 else ' = ', end = '')
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end='')
     tot *= c
-    c -= 1 
+    c -= 1
 print(end='' f'{tot}')
-print('','#' * 10,'')
+print('', '#' * 10, '')
 
-resultado=1
+resultado = 1
 
 print(f'{n}! = ', end='')
-for cont in range(1,n+1):
-    print(f'{cont}', end ='')
-    print(' x ' if cont < 10 else ' = ', end = '')
-    
+for cont in range(1, n+1):
+    print(f'{cont}', end='')
+    print(' x ' if cont < 10 else ' = ', end='')
+
     resultado *= cont
 
 print(resultado)
