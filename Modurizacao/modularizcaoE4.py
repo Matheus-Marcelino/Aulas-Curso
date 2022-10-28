0bfd5398f83520c71c0b549ae8eaf78cc2e4f0cb
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 17:39:48 -0300

    Finalizado

---
+++ Modurizacao/modularizcaoE4.py
@@ -0,0 +1,4 @@
+from moedaE4.moeda import resumo
+
+valor = float(input('Digite o preço: R$'))
+resumo(valor, 80, 35)
--- Modurizacao/moedaE4/moeda.py
+++ Modurizacao/moedaE4/moeda.py
@@ -16,3 +16,15 @@ def dobro(preco: float, ident: str = 'R$') -> str:
 
 def metade(preco: float, ident: str = 'R$') -> str:
     return moeda(preco / 2, ident)
+
+
+def resumo(valor: int, acrescimo: int, reducao: int) -> None:
+    print('-' * 30)
+    print(f'{"RESUMO DO VALOR":^30}')
+    print('-' * 30)
+    print(f'Preço analisado: {moeda(valor):>6}')
+    print(f'Dobro do preço: {dobro(valor):>6}')
+    print(f'Metade do preço: {metade(valor):>6}')
+    print(f'{acrescimo}% de aumento: {aumentar(valor, acrescimo):>6}')
+    print(f'{reducao}% de reducao: {diminuir(valor, reducao):>6}')
+    print('-' * 30)
