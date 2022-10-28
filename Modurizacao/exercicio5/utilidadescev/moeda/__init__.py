50e331c58e4f640e20b703372275a4205729ce66
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 18:10:18 -0300

    Finalizado

---
+++ Modurizacao/exercicio5/utilidadescev/moeda/__init__.py
@@ -0,0 +1,30 @@
+def moeda(preco: float, ident: str = 'R$') -> str:
+    return f'{ident}{preco:.2f}'.replace('.', ',')
+
+
+def aumentar(preco: float, taxa: float = 1, ident: str = 'R$') -> str:
+    return moeda(preco + (preco * taxa/100), ident)
+
+
+def diminuir(preco: float, taxa: float = 1, ident: str = 'R$') -> str:
+    return moeda(preco - (preco * taxa/100), ident)
+
+
+def dobro(preco: float, ident: str = 'R$') -> str:
+    return moeda(preco * 2, ident)
+
+
+def metade(preco: float, ident: str = 'R$') -> str:
+    return moeda(preco / 2, ident)
+
+
+def resumo(valor: int, acrescimo: int = 10, reducao: int = 5) -> None:
+    print('-' * 50)
+    print('RESUMO DO VALOR'.center(50))  # centraliza a string
+    print('-' * 50)
+    print(f'Preço analisado: \t{moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor)}')
+    print(f'Metade do preço: \t{metade(valor)}')
+    print(f'{acrescimo}% de aumento: \t{aumentar(valor, acrescimo)}')
+    print(f'{reducao}% de reducao: \t{diminuir(valor, reducao)}')
+    print('-' * 50)
---
+++ Modurizacao/modularizacaoE5.py
@@ -0,0 +1,4 @@
+from exercicio5.utilidadescev.moeda import resumo
+
+valor = float(input('Digite o preço: R$'))
+resumo(valor, 44, 25)
