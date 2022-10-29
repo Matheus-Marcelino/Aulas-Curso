dc1e441b404eee396f3a0dfef89554fb5cd1e862
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 22:44:12 -0300

    Finalizado

--- Tratamento de erro/tdeE2.py
+++ Tratamento de erro/tdeE2.py
@@ -1,8 +1,22 @@
 from requests import get
 
-url = "http://pudim.com.br/"
+url = 'http://pudim.com.br/'
 
 if get(url).status_code == 200:
     print("O servidor está disponível.")
 else:
     print("O servidor está indisponível.")
+
+
+"""
+-> maneira do professor
+
+import urllib.request as urgit 
+
+try:
+    site = ur.urlopen('http://pudim.com.br/')
+except:
+    print('Deu Erro')
+else:
+    print('tudo ok')
+"""
