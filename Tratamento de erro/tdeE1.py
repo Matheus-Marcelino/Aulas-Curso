bf9d5d092a61cbea51c0b8abee7322bfd1db11e4
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-28 21:49:40 -0300

    Exercicio finalizado

---
+++ Tratamento de erro/tdeE1.py
@@ -0,0 +1,23 @@
+def leiaInt(informacao: str) -> int:
+    while True:
+        try:
+            numero = int(input(informacao))
+            break
+        except ValueError:
+            print('Um erro de digitação ocorreu! tente denovo!')
+    return numero
+
+
+def leiafloat(informacao: str) -> float:
+    while True:
+        try:
+            numero = float(input(informacao))
+            break
+        except ValueError:
+            print('Um erro de digitação ocorreu! tente denovo!')
+    return numero
+
+
+inteiro = leiaInt('Digite um numero inteiro: ')
+real = leiafloat('Digite um número real: ')
+print(f'O números digitados foram: int:{inteiro}  Real:{real}')
