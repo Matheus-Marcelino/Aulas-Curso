54426ee3789da3d585303d5e96a198442fb88c80
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-10-30 13:35:40 -0300

    Finalizado

--- Tratamento de erro/tdeE3.py
+++ Tratamento de erro/tdeE3.py
@@ -6,31 +6,33 @@ while True:
     estilo('MENU PRINCIPAL', 40)
     print('\033[1;33m1 -\033[m \33[1;35mCadastrar nova Pessoa\033[m'
           '\n\033[1;33m2 -\033[m \033[1;35mVer pessoas cadastradas\033[m'
-          '\n\033[1;33m3 -\033[m \033[1;35mSair do programa\033[m'
-          '\n\033[1;33m4 -\033[m \033[1;35mLimpar Terminal\033[m')
+          '\n\033[1;33m3 -\033[m \033[1;35mLimpar Terminal\033[m'
+          '\n\033[1;33m4 -\033[m \033[1;35mSair do programa\033[m')
     decisao = leiaInt('Sua opção: ')
 
     if decisao == 1:
         estilo('CADASTRO', 40)
         cadastrar()
+        print('\033[1;32mCADASTRADO COM SUCESSO!\033[m')
     elif decisao == 2:
         estilo(" LOGGIN'S ", 40)
         datas = mostrar_cadastro()
         for i in range(len(datas)):
             for j in range(len(datas[i])):
                 if j % 2 == 0:
-                    print('{:<30}'.format(datas[i][j].replace('_', ' ')), end='')
+                    print('{:<30}'.format(
+                        datas[i][j].replace('_', ' ')), end='')
                 else:
                     print(f'{datas[i][j]} Anos')
-            print()
+            print(end='')
         sleep(2)
     elif decisao == 3:
+        clear()
+    elif decisao == 4:
         clear()
         print('\033[1;33mFinalizando...\033[m')
         sleep(2)
         break
-    elif decisao == 4:
-        clear()
     else:
         print(f'\033[1;31mCaracter "{decisao}" não foi reconhecido pelo sistema, '
               'porfavor faça uma escolha valida!\033[m')
