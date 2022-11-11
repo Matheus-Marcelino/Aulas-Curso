35f932c8a8318c2256fd0f35216a83ce0fc8c77b
Matheus Marcelino Lopes - DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-11-11 20:55:26 -0300

    DocString

---
+++ POO/classes_udemy.py
@@ -0,0 +1,18 @@
+"""
+Programação Orientada a Objetos - POO
+
+- POO é um paradigma de programação que utiliza que mapeamento
+de objetos do mundo real para modelos computacionais
+
+- Paradigama de programação é a forma/metodologia utilizada para
+pensar/desenvolver sistemas.
+
+Principais elementos da POO
+- Classe -> Modelo do objeto do mundo real sendo representado computacionalmente;
+- Atributo -> Caracteristicas do objeto.
+- Metodo -> Comportamento do objeto (funções).
+- Construtor -> Metodo especial utilizado para criar os objetos
+- Objetos -> Instância da classe
+"""
+
+
